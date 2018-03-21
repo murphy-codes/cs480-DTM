30f1fa777b1caca7e9703a9e3121693de6dcb12f
bravelemming <bravelemming@gmail.com>
2018-03-21 14:48:35 -0700

    full update!

--- web search testing/app/src/main/java/com/kinne/jack/websearchapi/MainActivity.java
+++ web search testing/app/src/main/java/com/kinne/jack/websearchapi/MainActivity.java
@@ -1,5 +1,7 @@
 package com.kinne.jack.websearchapi;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,45 +10,59 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText eText;
+    Button btn;
+    TextView resultTextView;
+
+    eText = (EditText) findViewById(R.id.edittext);
+    btn = (Button) findViewById(R.id.button);
+    resultTextView = (TextView) findViewById(R.id.textView1);
+
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        editTextInput = (EditText) findViewById(R.id.editTextInput);
+
+        // button onClick
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                final String searchString = editTextInput.getText().toString();
+
+                //resultTextView.setText("Searching for : " + searchString);
+                onSearchClick(v);
+
+
             }
         });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onSearchClick(View v)
+    {
+        try {
+            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            String term = editTextInput.getText().toString();
+            intent.putExtra(SearchManager.QUERY, term);
+            startActivity(intent);
+        } catch (Exception e) {
+            // TODO: handle exception
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+
+
 }
+
+
