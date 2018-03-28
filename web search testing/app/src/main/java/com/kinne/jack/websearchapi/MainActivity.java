0c3d314154860d4ba39c87ebe6ae09544819b223
bravelemming <bravelemming@gmail.com>
2018-03-28 14:53:17 -0700

    full update

--- web search testing/app/src/main/java/com/kinne/jack/websearchapi/MainActivity.java
+++ web search testing/app/src/main/java/com/kinne/jack/websearchapi/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,14 +15,53 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText eText;
     Button btn;
     TextView resultTextView;
 
+    String searchTerm = "matrix";
+    String returnString;
 
+    //send a RESTful request for google custom search
+    void googleSearch() {
+        //instance the requestQueue
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBuwIMBG5YjqxxkDsQEjz-el2xHw15RfFQ&cx=015559890765402091894:0yoxulceyae&q="+ searchTerm;
 
+        //request a string response from provided URL
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>(){
+            @Override
+            public void onResponse(JSONObject response) {
+                //display the first 500 characters of the response string.
+                String temp = "Response is: " + response.toString();
+                resultTextView.setText(temp);
+                returnString = temp;
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error){
+                String temp2 = "didn't work!";
+                resultTextView.setText(temp2);
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+    }
 
     /** Called when the activity is first created. */
     @Override
@@ -32,6 +72,8 @@ public class MainActivity extends AppCompatActivity {
         eText = (EditText) findViewById(R.id.editTextInput);
         btn = (Button) findViewById(R.id.button1);
         resultTextView = (TextView) findViewById(R.id.textView1);
+        //set textview as scrollable
+        resultTextView.setMovementMethod(new ScrollingMovementMethod());
 
         // button onClick
         btn.setOnClickListener(new View.OnClickListener() {
@@ -40,8 +82,10 @@ public class MainActivity extends AppCompatActivity {
                 final String searchString = eText.getText().toString();
 
                 //resultTextView.setText("Searching for : " + searchString);
-                onSearchClick(v);
+                //onSearchClick(v);
 
+                //call to custom search
+                googleSearch();
 
             }
         });
