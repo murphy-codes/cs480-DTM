c2c78774b16f8336c2028df87540194094e6423b
Thomas Murphy <trm405@humboldt.edu>
2018-04-30 06:20:21 -0700

    Main menu button added to settings

--- DTM/app/src/main/java/std/dtm/Settings.java
+++ DTM/app/src/main/java/std/dtm/Settings.java
@@ -1,14 +1,22 @@
+/*
+Author: Sam Alston, Tom Murphy, Jack (Daniel) Kinne [STD]
+Last Modified: 4/30/2018
+Purpose: Settings the view with which users select their settings
+ */
 package std.dtm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Switch;
 
 public class Settings extends AppCompatActivity {
 
     private Switch freeplayswitch;
     private Switch timerswitch;
+    private ImageButton mainMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,14 @@ public class Settings extends AppCompatActivity {
 
         freeplayswitch = (Switch) findViewById(R.id.freeplayswitch);
         timerswitch = (Switch) findViewById(R.id.timerswitch);
+        mainMenuButton = (ImageButton) findViewById(R.id.buttonMainMenu);
+
+        mainMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMainMenu();
+            }
+        });
 
         if(MainActivity.settings.isFreePlaySet()) {
             freeplayswitch.setChecked(true);
@@ -39,5 +55,12 @@ public class Settings extends AppCompatActivity {
                 MainActivity.settings.setTimer(timerswitch.isChecked());
             }
         });
+    }
+
+    //set onclick Listener to return to MainActivity
+    private void goToMainMenu() {
+        final Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
