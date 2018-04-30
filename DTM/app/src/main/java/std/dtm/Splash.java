4cd2fcde0bed7252797a4f78f9081da18e8c5e5c
Thomas Murphy <trm405@humboldt.edu>
2018-04-30 02:57:28 -0700

    Updates to splash screen

--- DTM/app/src/main/java/std/dtm/Splash.java
+++ DTM/app/src/main/java/std/dtm/Splash.java
@@ -1,7 +1,7 @@
 /*
 Authors: Sam Alston, Tom Murphy, Jack (Daniel) Kinne [STD]
 Last Modified: 4/28/2018
-Purpose: GetAnswer Displays the movie that the DTM logo for a short period of time, before transitioning to MainActivity
+Purpose: Splash displays the DTM (splash) logo for a short period of time, before transitioning to MainActivity
  */
 package std.dtm;
 
@@ -15,21 +15,30 @@ import android.widget.ImageView;
 public class Splash extends AppCompatActivity {
 
     private ImageView splashLogo;
+    private ImageView developedBy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         splashLogo = (ImageView) findViewById(R.id.imageViewSplashLogo);
-        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.splash_transition_in);
+        developedBy = (ImageView) findViewById(R.id.imageViewDevelopedBy);
+        Animation fadeInLogo = AnimationUtils.loadAnimation(this, R.anim.splash_transition_in);
+        Animation fadeInDevBy = AnimationUtils.loadAnimation(this, R.anim.dev_by_transition_in);
+        //final Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.splash_transition_out);
         // For implementing a fade out to follow the fade in, later.
         // Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.splash_transition_out);
-        splashLogo.startAnimation(fadeIn);
+        splashLogo.startAnimation(fadeInLogo);
+        developedBy.startAnimation(fadeInDevBy);
         final Intent i = new Intent(this, MainActivity.class);
         Thread timer = new Thread(){
             public void run (){
                 try {
-                    sleep(4500);
+                    //sleep(4000);
+                    //splashLogo.startAnimation(fadeOut);
+                    //developedBy.startAnimation(fadeOut);
+                    //sleep(1500);
+                    sleep(5500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
